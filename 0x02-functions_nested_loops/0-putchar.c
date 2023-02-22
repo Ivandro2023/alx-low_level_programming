f04d53311fa16f37cb5e55c54c78b2a5ae875c59
IVANDRO <pinaivandro21@gmail.com>
2023-02-22 05:07:06 -0800

    0x02_EXO_0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#ifndef _PUTCHAR_C_
-#define _PUTCHAR_C_
-
 #include "main.h"
 
 /**
- * _putchar - prints a character to stdout
- * @c: the character to print
- *
- * Return: 1 on success, -1 on error.
- */
-int _putchar(char c)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-  return (write(1, &c, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
-
-#endif /* _PUTCHAR_C_ */
-
