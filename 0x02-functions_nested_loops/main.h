e6043e5bb41d1eded82a4bca343015bf9d11097a
IVANDRO <pinaivandro21@gmail.com>
2023-02-22 00:09:26 -0800

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,5 @@
-#include <stdio.h>
-
-int main(void) {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return 0;
+#include <unistd.h>
+Int  _putchar(char c)
+{
+Return (write(1, &c, 1);
 }
-
