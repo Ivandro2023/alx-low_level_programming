5132aa1cff494886a82e2c13a44a42b21ca5dfb0
IVANDRO <pinaivandro21@gmail.com>
2023-02-22 01:52:44 -0800

    0x02_EXO_0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
-* The main function of the program.
-* This function prints the word "putchar" character by character, followed by a newline.
-*
-* @return 0 if the program runs successfully.
-*/
-
 int main(void)
 
-//code for printing "putchar" character by character goes here
 {
 putchar('_');
 putchar('p');
